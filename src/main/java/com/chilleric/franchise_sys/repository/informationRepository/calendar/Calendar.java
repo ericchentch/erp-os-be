c366bfba5c23d0ba59cba5745356677e3d7be1d2
AdenTroy <adentroy.tch@gmail.com>
2022-12-25 22:33:22 +0700

    init: calendar repository

---
+++ src/main/java/com/chilleric/franchise_sys/repository/informationRepository/calendar/Calendar.java
@@ -0,0 +1,24 @@
+package com.chilleric.franchise_sys.repository.informationRepository.calendar;
+
+import java.util.Date;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "calendars")
+public class Calendar {
+    private ObjectId _id;
+
+    private ObjectId roomTypeId;
+    private String position;
+    private ObjectId billId;
+
+    private Date checkoutTime;
+
+    private Date checkinTime;
+}
