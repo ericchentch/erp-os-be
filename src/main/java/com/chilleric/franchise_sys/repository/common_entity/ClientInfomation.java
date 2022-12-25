d16377520a535b73f4a6e1a00e25fd77bd2654a8
AdenTroy <adentroy.tch@gmail.com>
2022-12-25 17:10:37 +0700

    init: hotel repository

---
+++ src/main/java/com/chilleric/franchise_sys/repository/common_entity/ClientInfomation.java
@@ -0,0 +1,13 @@
+package com.chilleric.franchise_sys.repository.common_entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientInfomation {
+    String address;
+    String phone;
+}
---
+++ src/main/java/com/chilleric/franchise_sys/repository/informationRepository/hotel/HotelRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.chilleric.franchise_sys.repository.informationRepository.hotel;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.data.mongodb.core.query.Query;
+import com.chilleric.franchise_sys.repository.AbstractRepo;
+
+public class HotelRepositoryImpl extends AbstractRepo implements HotelRepository {
+
+    @Override
+    public void insertAndUpdate(Hotel hotel) {
+        informationDBTemplate.save(hotel, "hotels");
+    }
+
+    @Override
+    public long getTotalPage(Map<String, String> allParams) {
+        Query query = generateQueryMongoDB(allParams, Hotel.class, "", "", 0, 0);
+        informationDBTemplate.count(query, Hotel.class);
+        return 0;
+    }
+
+    @Override
+    public Optional<List<Hotel>> getHotels(Map<String, String> allParams, String keySort, int page,
+            int pageSize, String sortField) {
+        Query query =
+                generateQueryMongoDB(allParams, Hotel.class, keySort, sortField, page, pageSize);
+
+        return informationFind(query, Hotel.class);
+    }
+
+}
