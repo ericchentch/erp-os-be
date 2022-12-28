263b83b561cd7fa64f4ccd452e7189ba1cce9cae
AdenTroy <adentroy.tch@gmail.com>
2022-12-28 13:57:57 +0700

    implement hotel service

---
+++ src/main/java/com/chilleric/franchise_sys/service/hotel/HotelService.java
@@ -0,0 +1,17 @@
+package com.chilleric.franchise_sys.service.hotel;
+
+import java.util.Optional;
+import com.chilleric.franchise_sys.dto.hotel.HotelRequest;
+import com.chilleric.franchise_sys.dto.hotel.HotelResponse;
+
+public interface HotelService {
+
+    void createNewHotel(HotelRequest hotelRequest);
+
+    void updateHotel(String hotelId, HotelRequest hotelRequest);
+
+    Optional<HotelResponse> getHotelById(String hotelId);
+
+    void deleteHotel(String hotelId);
+}
+
