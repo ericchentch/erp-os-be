8b2c754f88e1deb146fc2162b9486354f18f476c
AdenTroy <adentroy.tch@gmail.com>
2022-12-30 01:06:38 +0700

    update roomTypeDto + roomType entity

--- src/main/java/com/chilleric/franchise_sys/repository/informationRepository/roomType/RoomType.java
+++ src/main/java/com/chilleric/franchise_sys/repository/informationRepository/roomType/RoomType.java
@@ -15,7 +15,8 @@ public class RoomType {
     private ObjectId _id;
     private ObjectId hotelId;
     private String name;
-    private List<String> imageLinks;
+    private List<String> linkImages;
+    private List<String> rooms;
     private float rate;
     private float stockPrice;
 }
