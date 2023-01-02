46e2d84b1e03cb44122e2330ba43730bc34f51d1
Aden Troy <adentroy.tch@gmail.com>
2023-01-02 14:52:42 +0700

    change name in repository

--- src/main/java/com/chilleric/franchise_sys/repository/informationRepository/shift/Shift.java
+++ src/main/java/com/chilleric/franchise_sys/repository/informationRepository/shift/Shift.java
@@ -1,12 +1,11 @@
 package com.chilleric.franchise_sys.repository.informationRepository.shift;
 
+import java.util.Date;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +14,6 @@ import java.util.Date;
 public class Shift {
     private ObjectId _id;
     private String shiftName;
-    private Date startHour;
-    private Date endHour;
+    private Date startTime;
+    private Date endTime;
 }
