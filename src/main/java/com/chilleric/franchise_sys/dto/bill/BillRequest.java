969ecaf395f98c5982da1c4dfc85adf0a0de33f5
Aden Troy <adentroy.tch@gmail.com>
2023-01-02 14:02:08 +0700

    update required for reques

--- src/main/java/com/chilleric/franchise_sys/dto/bill/BillRequest.java
+++ src/main/java/com/chilleric/franchise_sys/dto/bill/BillRequest.java
@@ -1,19 +1,33 @@
 package com.chilleric.franchise_sys.dto.bill;
 
+import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import com.chilleric.franchise_sys.constant.LanguageMessageKey;
 import com.chilleric.franchise_sys.repository.common_enum.BillStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BillRequest {
+    @NotBlank(message = LanguageMessageKey.BILL_CUSTOMER_ID_REQUIRED)
+    @NotEmpty(message = LanguageMessageKey.BILL_CUSTOMER_ID_REQUIRED)
+    @NotNull(message = LanguageMessageKey.BILL_CUSTOMER_ID_REQUIRED)
     private String customerId;
+
+    @NotNull(message = LanguageMessageKey.BILL_DRAFT_DETAIL_REQUIRED)
+    @NotEmpty(message = LanguageMessageKey.BILL_DRAFT_DETAIL_REQUIRED)
     private List<DraftDetailRequest> draftDetail;
+
+    @NotNull(message = LanguageMessageKey.BILL_TIMELINE_REQUIRED)
     private List<TimelineRequest> timeline;
 
+    @NotNull(message = LanguageMessageKey.BILL_STATUS_REQUIRED)
+    @NotEmpty(message = LanguageMessageKey.BILL_STATUS_REQUIRED)
+    @NotBlank(message = LanguageMessageKey.BILL_STATUS_REQUIRED)
     private BillStatus status;
 }
