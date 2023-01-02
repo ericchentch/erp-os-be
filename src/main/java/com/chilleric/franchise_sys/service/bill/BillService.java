91e5169cd87572f7b3346aecdf0959dd58421d30
Aden Troy <adentroy.tch@gmail.com>
2023-01-02 14:04:37 +0700

    init bill service

--- src/main/java/com/chilleric/franchise_sys/service/bill/BillService.java
+++ src/main/java/com/chilleric/franchise_sys/service/bill/BillService.java
@@ -1,7 +1,25 @@
 package com.chilleric.franchise_sys.service.bill;
 
 import com.chilleric.franchise_sys.dto.bill.BillRequest;
+import com.chilleric.franchise_sys.dto.bill.BillResponse;
+import com.chilleric.franchise_sys.repository.common_entity.DraftDetail;
+import com.chilleric.franchise_sys.repository.common_entity.Timeline;
+import com.chilleric.franchise_sys.repository.common_enum.BillStatus;
+import com.chilleric.franchise_sys.repository.crmRepository.bill.Bill;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface BillService {
-    void addItemIntoDraftBill(BillRequest billRequest);
+    void createBill(BillRequest billRequest);
+
+    void updateBill(BillRequest billRequest, String billId);
+
+    Optional<BillResponse> getBillResponseById(String billId);
+
+    Optional<Bill> findBillById(String billId);
+
+    BillStatus getBillStatus(List<Timeline> timelineList, List<DraftDetail> draftDetailList);
+
+
 }
