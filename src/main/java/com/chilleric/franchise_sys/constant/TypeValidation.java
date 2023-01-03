4abb591607faaf0a7a9833bac1d5b9511eef9bf8
Aden Troy <adentroy.tch@gmail.com>
2023-01-03 18:04:58 +0700

    update validate hour shift request

--- src/main/java/com/chilleric/franchise_sys/constant/TypeValidation.java
+++ src/main/java/com/chilleric/franchise_sys/constant/TypeValidation.java
@@ -7,6 +7,7 @@ public class TypeValidation {
       "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-!@#&()[{}]:;',?/*~$^+=<>]).{8,20}$";
   public static final String PHONE = "^(((\\+|)84)|0)(3|5|7|8|9)+([0-9]{8})$";
   public static final String DATE = "^\\d{4}-\\d{2}-\\d{2}$";
+  public static final String HOUR = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
   public static final String EMAIL =
       "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
   public static final String BASE64_REGEX = "^[a-zA-Z0-9-!@#&()[{}]:;',?/*~$^+=<>]+$";
