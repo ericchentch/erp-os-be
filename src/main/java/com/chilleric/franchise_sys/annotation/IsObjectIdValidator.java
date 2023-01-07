15591ccd223998c033bec5a9b9197149f10de179
Aden Troy <adentroy.tch@gmail.com>
2023-01-08 02:34:05 +0700

    create annotation for validate ObjectId in dto

---
+++ src/main/java/com/chilleric/franchise_sys/annotation/IsObjectIdValidator.java
@@ -0,0 +1,19 @@
+package com.chilleric.franchise_sys.annotation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import org.bson.types.ObjectId;
+
+public class IsObjectIdValidator implements ConstraintValidator<IsObjectId, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (ObjectId.isValid(value)) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+
+}
