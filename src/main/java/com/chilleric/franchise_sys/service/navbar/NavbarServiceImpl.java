bce7cb83f0b5c4f22ad0648902a08c06b1f8d2a2
ericchentch <ericchen.tch@gmail.com>
2023-01-16 13:44:45 +0700

    update-logic-navbar

--- src/main/java/com/chilleric/franchise_sys/service/navbar/NavbarServiceImpl.java
+++ src/main/java/com/chilleric/franchise_sys/service/navbar/NavbarServiceImpl.java
@@ -13,12 +13,16 @@ import com.chilleric.franchise_sys.constant.LanguageMessageKey;
 import com.chilleric.franchise_sys.dto.common.ListWrapperResponse;
 import com.chilleric.franchise_sys.dto.navbar.NavbarRequest;
 import com.chilleric.franchise_sys.dto.navbar.NavbarResponse;
+import com.chilleric.franchise_sys.dto.path.PathResponse;
 import com.chilleric.franchise_sys.exception.InvalidRequestException;
 import com.chilleric.franchise_sys.exception.ResourceNotFoundException;
 import com.chilleric.franchise_sys.inventory.navbar.NavbarInventory;
+import com.chilleric.franchise_sys.inventory.path.PathInventory;
+import com.chilleric.franchise_sys.inventory.user.UserInventory;
 import com.chilleric.franchise_sys.repository.systemRepository.accessability.Accessability;
 import com.chilleric.franchise_sys.repository.systemRepository.navbar.Navbar;
 import com.chilleric.franchise_sys.repository.systemRepository.navbar.NavbarRepository;
+import com.chilleric.franchise_sys.repository.systemRepository.path.Path;
 import com.chilleric.franchise_sys.service.AbstractService;
 
 @Service
@@ -27,6 +31,12 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
     @Autowired
     private NavbarInventory navbarInventory;
 
+    @Autowired
+    private PathInventory pathInventory;
+
+    @Autowired
+    private UserInventory userInventory;
+
     @Override
     public Optional<ListWrapperResponse<NavbarResponse>> getListNavbar(
             Map<String, String> allParams, String keySort, int page, int pageSize, String sortField,
@@ -34,15 +44,33 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
         List<Navbar> navbarList =
                 repository.getNavbarList(allParams, keySort, page, pageSize, sortField).get();
         return Optional
-                .of(new ListWrapperResponse<>(
-                        navbarList.stream().map(navbar -> new NavbarResponse(
-                                navbar.get_id().toString(), navbar.getName(),
+                .of(new ListWrapperResponse<>(navbarList.stream()
+                        .map(navbar -> new NavbarResponse(navbar.get_id().toString(),
+                                navbar.getName(),
                                 navbar.getUserIds().stream().map(thisId -> thisId.toString())
                                         .collect(Collectors.toList()),
-                                navbar.getContents().stream().map(thisId -> thisId.toString())
-                                        .collect(Collectors.toList())))
-                                .collect(Collectors.toList()),
-                        page, pageSize, repository.getTotal(allParams)));
+                                navbar.getMainSidebar().stream()
+                                        .filter(thisPath -> pathInventory
+                                                .findPathById(thisPath.toString()).isPresent())
+                                        .map(thisPath -> {
+                                            Path path = pathInventory
+                                                    .findPathById(thisPath.toString()).get();
+                                            return new PathResponse(path.get_id().toString(),
+                                                    path.getLabel(), path.getPath(), path.getType(),
+                                                    path.getIcon());
+                                        }).collect(Collectors.toList()),
+                                navbar.getMainSidebar().stream()
+                                        .filter(thisPath -> pathInventory
+                                                .findPathById(thisPath.toString()).isPresent())
+                                        .map(thisPath -> {
+                                            Path path = pathInventory
+                                                    .findPathById(thisPath.toString()).get();
+                                            return new PathResponse(path.get_id().toString(),
+                                                    path.getLabel(), path.getPath(), path.getType(),
+                                                    path.getIcon());
+                                        }).collect(Collectors.toList())))
+                        .collect(Collectors.toList()), page, pageSize,
+                        repository.getTotal(allParams)));
     }
 
     @Override
@@ -52,8 +80,21 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
         return Optional.of(new NavbarResponse(navbar.get_id().toString(), navbar.getName(),
                 navbar.getUserIds().stream().map(thisId -> thisId.toString())
                         .collect(Collectors.toList()),
-                        navbar.getContents().stream().map(thisId -> thisId.toString())
-                        .collect(Collectors.toList())));
+
+                navbar.getMainSidebar().stream().filter(
+                        thisPath -> pathInventory.findPathById(thisPath.toString()).isPresent())
+                        .map(thisPath -> {
+                            Path path = pathInventory.findPathById(thisPath.toString()).get();
+                            return new PathResponse(path.get_id().toString(), path.getLabel(),
+                                    path.getPath(), path.getType(), path.getIcon());
+                        }).collect(Collectors.toList()),
+                navbar.getMainSidebar().stream().filter(
+                        thisPath -> pathInventory.findPathById(thisPath.toString()).isPresent())
+                        .map(thisPath -> {
+                            Path path = pathInventory.findPathById(thisPath.toString()).get();
+                            return new PathResponse(path.get_id().toString(), path.getLabel(),
+                                    path.getPath(), path.getType(), path.getIcon());
+                        }).collect(Collectors.toList())));
     }
 
     @Override
@@ -63,8 +104,21 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
         return Optional.of(new NavbarResponse(navbar.get_id().toString(), navbar.getName(),
                 navbar.getUserIds().stream().map(thisId -> thisId.toString())
                         .collect(Collectors.toList()),
-                navbar.getContents().stream().map(thisId -> thisId.toString())
-                        .collect(Collectors.toList())));
+
+                navbar.getMainSidebar().stream().filter(
+                        thisPath -> pathInventory.findPathById(thisPath.toString()).isPresent())
+                        .map(thisPath -> {
+                            Path path = pathInventory.findPathById(thisPath.toString()).get();
+                            return new PathResponse(path.get_id().toString(), path.getLabel(),
+                                    path.getPath(), path.getType(), path.getIcon());
+                        }).collect(Collectors.toList()),
+                navbar.getMainSidebar().stream().filter(
+                        thisPath -> pathInventory.findPathById(thisPath.toString()).isPresent())
+                        .map(thisPath -> {
+                            Path path = pathInventory.findPathById(thisPath.toString()).get();
+                            return new PathResponse(path.get_id().toString(), path.getLabel(),
+                                    path.getPath(), path.getType(), path.getIcon());
+                        }).collect(Collectors.toList())));
     }
 
     @Override
@@ -79,18 +133,27 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
         });
         List<ObjectId> userIdsUpdate = new ArrayList<>();
         navbarRequest.getUserIds().forEach(thisId -> {
-            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()) {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && userInventory.findUserById(thisId.toString()).isPresent()) {
                 userIdsUpdate.add(new ObjectId(thisId));
             }
         });
-        List<ObjectId> contentUpdate = new ArrayList<>();
-        navbarRequest.getContent().forEach(thisId -> {
-            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()) {
-                contentUpdate.add(new ObjectId(thisId));
+        List<ObjectId> mainUpdate = new ArrayList<>();
+        navbarRequest.getMainSidebar().forEach(thisId -> {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && pathInventory.findPathById(thisId).isPresent()) {
+                mainUpdate.add(new ObjectId(thisId));
+            }
+        });
+        List<ObjectId> childrenUpdate = new ArrayList<>();
+        navbarRequest.getChildrenSidebar().forEach(thisId -> {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && pathInventory.findPathById(thisId).isPresent()) {
+                childrenUpdate.add(new ObjectId(thisId));
             }
         });
         repository.insertAndUpdate(new Navbar(new ObjectId(id), navbarRequest.getName(),
-                userIdsUpdate, contentUpdate));
+                userIdsUpdate, mainUpdate, childrenUpdate));
     }
 
     @Override
@@ -103,21 +166,30 @@ public class NavbarServiceImpl extends AbstractService<NavbarRepository> impleme
         });
         List<ObjectId> userIdsUpdate = new ArrayList<>();
         navbarRequest.getUserIds().forEach(thisId -> {
-            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()) {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && userInventory.findUserById(thisId.toString()).isPresent()) {
                 userIdsUpdate.add(new ObjectId(thisId));
             }
         });
-        List<ObjectId> contentUpdate = new ArrayList<>();
-        navbarRequest.getContent().forEach(thisId -> {
-            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()) {
-                contentUpdate.add(new ObjectId(thisId));
+        List<ObjectId> mainUpdate = new ArrayList<>();
+        navbarRequest.getMainSidebar().forEach(thisId -> {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && pathInventory.findPathById(thisId).isPresent()) {
+                mainUpdate.add(new ObjectId(thisId));
+            }
+        });
+        List<ObjectId> childrenUpdate = new ArrayList<>();
+        navbarRequest.getChildrenSidebar().forEach(thisId -> {
+            if (accessabilityRepository.getAccessability(loginId, thisId).isPresent()
+                    && pathInventory.findPathById(thisId).isPresent()) {
+                childrenUpdate.add(new ObjectId(thisId));
             }
         });
         ObjectId newId = new ObjectId();
         accessabilityRepository
                 .addNewAccessability(new Accessability(null, new ObjectId(loginId), newId, true));
-        repository.insertAndUpdate(
-                new Navbar(newId, navbarRequest.getName(), userIdsUpdate, contentUpdate));
+        repository.insertAndUpdate(new Navbar(newId, navbarRequest.getName(), userIdsUpdate,
+                mainUpdate, childrenUpdate));
     }
 
     @Override
